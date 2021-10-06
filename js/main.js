1fde044459c3529c15a08f650e04e2298ac5aef5
rajvpatil5 <48220124+rajvpatil5@users.noreply.github.com>
2021-10-06 22:35:44 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+"use strict";
+let lng, lat;
+const cityName = document.getElementById("city-name");
+const searchCity = document.getElementById("search-city");
+const tempDegree = document.querySelector(".temperature-degree");
+const tempDiv = document.querySelector(".temperature");
+const timeZone = document.querySelector(".location-timezone");
+const apiKey = "d3fbf2b1bd263206c599d1c0f2568eae";
+
+console.log(cityName);
+
+searchCity.addEventListener("click", function () {
+  let insertedCityName = cityName.value;
+  console.log(insertedCityName);
+  getCityData(insertedCityName);
+  // cityName.value = "";
+});
+
+const time = 19800 / 60 / 60;
+console.log(Intl.DateTimeFormat().resolvedOptions());
+
+const getCityData = async function (insCity) {
+  try {
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${insCity}&appid=${apiKey}`
+    );
+    const response = await request.json();
+    console.log(response);
+
+    //gaurd class
+    if (!response) return;
+
+    renderHtml(response);
+  } catch (error) {
+    console.error(new Error("Error while fetching"));
+  }
+};
+
+const renderHtml = function (response) {
+  const { temp } = response.main;
+  tempDegree.innerHTML = temp;
+
+  timeZone.textContent = `Timezone - 
+  UTC${response.timezone / 60 / 60} hrs`;
+  const html = response.weather
+    .map(
+      (ele) =>
+        `<div class="temperature-description">
+      ${ele.main} - ${ele.description}
+    </div>`
+    )
+    .join("");
+  // tempDiv.innerHTML = "";
+  tempDiv.insertAdjacentHTML("beforeend", html);
+};
+/*
+https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=d3fbf2b1bd263206c599d1c0f2568eae
+*/
